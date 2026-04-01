bfacbf673648f5448bb6f36075306a7d8ab6124e
M Samarth Mahendra <samarth.mahendragowda@gmail.com>
2026-04-01 19:23:49 -0400

    solved down speed

--- script.js
+++ script.js
@@ -475,8 +475,8 @@ document.addEventListener('DOMContentLoaded', function () {
                 el: el,
                 x: Math.random() * (window.innerWidth - size),
                 y: Math.random() * (window.innerHeight - size),
-                vx: (Math.random() < 0.5 ? -1 : 1) * (0.4 + Math.random() * 0.4),
-                vy: (Math.random() < 0.5 ? -1 : 1) * (0.4 + Math.random() * 0.4),
+                vx: (Math.random() < 0.5 ? -1 : 1) * (0.2 + Math.random() * 0.2),
+                vy: (Math.random() < 0.5 ? -1 : 1) * (0.2 + Math.random() * 0.2),
                 radius: radius,
                 mass: 1
             });
